fc2f977fd3ac888b332e9b2f0e3d3224a155f6ab
Harshal <harshal.limaye@outlook.com>
2026-09-01 21:19:29 +0530

    feat(cli): show root folder and git branch in home status bar

--- packages/cli/src/screens/home.tsx
+++ packages/cli/src/screens/home.tsx
@@ -1,9 +1,13 @@
 import { useTheme } from '../providers/theme';
 import { TextAttributes } from '@opentui/core';
 import { InputBar } from '../components/input-bar';
+import { getCurrentBranch } from '../utils/git';
+import { getRootFolderName } from '@codeyantram/shared';
 
 export function Home() {
     const { colors } = useTheme();
+    const branch = getCurrentBranch();
+    const rootFolderName = getRootFolderName();
 
     return (
         <box backgroundColor={colors.bg} alignItems="center" justifyContent="center" flexGrow={1}>
@@ -15,15 +19,9 @@ export function Home() {
                 <InputBar />
             </box>
             <box flexDirection="row" width={60} justifyContent="space-between" marginTop={1}>
-                <box flexDirection="row" gap={2}>
-                    <box flexDirection="row" gap={1}>
-                        <text>tab</text>
-                        <text attributes={TextAttributes.DIM}>agent</text>
-                    </box>
-                    <box flexDirection="row" gap={1}>
-                        <text>ctrl+c</text>
-                        <text attributes={TextAttributes.DIM}>exit</text>
-                    </box>
+                <box flexDirection="row" gap={1}>
+                    <box><text>{rootFolderName}</text></box>
+                    <box><text attributes={TextAttributes.DIM}>git:{branch}</text></box>                    
                 </box>
                 <box>
                     <text attributes={TextAttributes.DIM}>v1.0.0</text>
